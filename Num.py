2e653d75365ff6acaab6a7d48bf3dbeb406f2455
aamir <aamir@gmail.com>
2022-11-03 12:34:45 +0530

    git status

---
+++ Num.py
@@ -0,0 +1,314 @@
+import numpy as np
+
+# myarray = np.array([[1, 2, 3], [4, 5, 6], [0, 3, 1]])
+# print(myarray.dtype)
+# print(myarray.shape)
+# print(myarray.size)
+# myarray[2, 1] = 10
+# print(myarray) 
+
+
+# zeros = np.zeros(100)
+# print(zeros)
+
+
+# Range = np.arange(15)
+# print(Range)
+
+
+# lspace = np.linspace(1, 4, 12)
+# print(lspace)
+
+
+# emp = np.empty((4, 6))
+# print(emp)
+
+
+# emp_like = np.empty_like(Range)
+# print(emp_like)
+
+
+# ide = np.identity(4)
+
+# print(ide)
+# print(int(ide.dtype))
+
+# myarr = np.arange(99)
+# print(myarr)
+
+# a = myarr.reshape(3,33)
+# a[2,3] = 10
+
+# b=a.ravel() 
+# c=a.shape
+# print(c)
+# print(b)
+
+
+Axiss = np.array([[2,2,4,5,3],[3,2,4,6,3],[2,4,5,4,3]])
+my=Axiss.sum(axis=1)
+print(my)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ******************** Slicing Array *************************
+
+
+# arr = np.array([1, 2, 3, 4, 5, 6, 7])
+# print(arr[1:6])
+# print(arr[3:])
+# print(arr[:3])
+# print(arr[-4:-2])
+# print(arr[-4:-2])
+
